a57b9964c2e2b9a15cba772689c4fa06081994bd
reker- <reker-@users.noreply.github.com>
2015-07-10 22:39:49 +0200

    Change project structure [ci skip]

--- tools/build_js/settings.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	PATH_COMPILER		: "../../../compiler/spcomp.exe",
-	PATH_SP_INCLUDE		: "../../../include/",
-	PATH_SP_SRC			: "../../src/",
-	PATH_SP_BIN			: "../../bin/",
-	PATH_SP_REL			: "../../release/",
-	PATH_SP_DEP			: "../../dependencies/",
-	COMP_FLAGS			: ""
-};
--- tools/build_js/settings.travis.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	PATH_COMPILER		: "/addons/sourcemod/scripting/spcomp",
-	PATH_SP_INCLUDE		: "../../addons/sourcemod/scripting/include/",
-	PATH_SP_SRC			: "../../src/",
-	PATH_SP_BIN			: "../../bin/",
-	PATH_SP_REL			: "../../release/",
-	PATH_SP_DEP			: "../../dependencies/",
-	COMP_FLAGS			: ""
-};
